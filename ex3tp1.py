8726a97fd0a2f998b1cd546d5ce5b396dbe1261f
Bilalzinini <zininibilal3@gmail.com>
2025-11-17 12:02:15 +0100

    'equation admet

---
+++ ex3tp1.py
@@ -0,0 +1,12 @@
+a=int(input("entrez la valure de A : "))
+b=int(input("entrez la valure de B : "))
+c=int(input("entrez la valure de C : "))
+d=b**2-4*a*c
+if d>0:
+    print("l'equation admet deux solution")
+elif d<0:
+    print("l'equation admet aucun solution")
+else :
+    print("l'equation admet une  solution")
+   
+    
