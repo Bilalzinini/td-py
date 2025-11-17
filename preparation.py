4bcbc60e7ff334066032dc4a9427a8e7cb8aed4f
Bilalzinini <zininibilal3@gmail.com>
2025-11-17 11:19:45 +0100

    come

---
+++ preparation.py
@@ -0,0 +1,24 @@
+a=3<5
+print(a)
+a= 2!=4
+print(a)
+a=1>8
+print(a)
+a=(500%30)==20
+print(a)
+a=(18*20)<(29**10)
+print(a)
+c=(1<6)and(2+6==8)
+print(c)
+a=5
+b=6
+c=(a<b)and(3<b)
+print(c)
+d=(str(a=5))or(b>10)
+print(d)
+e=not c
+print(e)
+f=c or (e and d )
+print(f)
+g=(not(e)and f)or (c and d)
+print(g)
